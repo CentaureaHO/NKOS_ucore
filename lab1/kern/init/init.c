76ffe898547b9bba32aaf4f21da30e9504eee436
varyhoo <837880374@qq.com>
2024-09-28 17:20:16 +0800

    add reference

--- lab1/kern/init/init.c
+++ lab1/kern/init/init.c
@@ -14,15 +14,17 @@ int kern_init(void) __attribute__((noreturn));
 void grade_backtrace(void);
 
 int kern_init(void) {
-    extern char edata[], end[];
-    memset(edata, 0, end - edata);
+    extern char edata[], end[];//声明了外部符号 edata 和 end，它们指向内存中某些特定位置，通常用于标识内核不同内存段的边界：
+    //edata：已初始化的数据段（data segment）的结束地址。
+    //end：未初始化的数据段（BSS segment）的结束地址。
+    memset(edata, 0, end - edata);//用来清空BSS段（BSS段保存未初始化的全局变量），确保这些变量在程序开始执行时都是零。
 
     cons_init();  // init the console
 
     const char *message = "(THU.CST) os is loading ...\n";
     cprintf("%s\n\n", message);
 
-    print_kerninfo();
+    print_kerninfo();//这一行调用 print_kerninfo 函数，输出内核的特定符号信息，比如内核入口、代码段结束、数据段结束等地址，帮助调试内核内存布局。
 
     // grade_backtrace();
 
