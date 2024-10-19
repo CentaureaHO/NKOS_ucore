9b9f44e2adf73dafdf2a3a44c477f6283a13a622
王禹衡 <837880374@qq.com>
2024-10-19 21:39:11 +0800

    处理运行问题

--- lab2/libs/sbi.c
+++ lab2/libs/sbi.c
@@ -31,3 +31,5 @@ void sbi_console_putchar(unsigned char ch) { sbi_call(SBI_CONSOLE_PUTCHAR, ch, 0
 void sbi_set_timer(unsigned long long stime_value) { sbi_call(SBI_SET_TIMER, stime_value, 0, 0); }
 
 int sbi_console_getchar(void) { return sbi_call(SBI_CONSOLE_GETCHAR, 0, 0, 0); }
+
+void sbi_shutdown(void) { sbi_call(SBI_SHUTDOWN, 0, 0, 0); }
