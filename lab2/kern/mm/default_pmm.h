8f52a2a550b77acaed0227f10a71c2e4f7980b5a
王禹衡 <837880374@qq.com>
2024-10-20 23:29:00 +0800

    完成buddy_system

--- lab2/kern/mm/default_pmm.h
+++ lab2/kern/mm/default_pmm.h
@@ -5,4 +5,10 @@
 
 extern const struct pmm_manager default_pmm_manager;
 
+static void         default_init(void);
+static void         default_init_memmap(struct Page* base, size_t n);
+static struct Page* default_alloc_pages(size_t n);
+static void         default_free_pages(struct Page* base, size_t n);
+static size_t       default_nr_free_pages(void);
+static void         default_check(void);
 #endif /* ! __KERN_MM_DEFAULT_PMM_H__ */
