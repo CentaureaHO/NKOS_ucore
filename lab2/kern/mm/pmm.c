d12af4f370ec5a26eb6334a2994970704abbb73e
CentaureaHO <tangxianda15@outlook.com>
2024-10-27 08:19:41 +0800

    通过grade需要使用best-fit

--- lab2/kern/mm/pmm.c
+++ lab2/kern/mm/pmm.c
@@ -36,7 +36,7 @@ static void check_alloc_page(void);
 // init_pmm_manager - initialize a pmm_manager instance
 static void init_pmm_manager(void)
 {
-    pmm_manager = &slub_pmm_manager;
+    pmm_manager = &best_fit_pmm_manager;
     cprintf("memory management: %s\n", pmm_manager->name);
     pmm_manager->init();
 }
