94c2af338c8be1e0a6509fd47c935f54d09ec283
王禹衡 <837880374@qq.com>
2024-10-30 09:58:43 +0800

    完成exercise4

--- lab3/kern/mm/swap_clock.c
+++ lab3/kern/mm/swap_clock.c
@@ -37,7 +37,9 @@ static int _clock_init_mm(struct mm_struct* mm)
     // 初始化当前指针curr_ptr指向pra_list_head，表示当前页面替换位置为链表头
     // 将mm的私有成员指针指向pra_list_head，用于后续的页面替换算法操作
     // cprintf(" mm->sm_priv %x in fifo_init_mm\n",mm->sm_priv);
-    
+    list_init(&pra_list_head);
+    curr_ptr = &pra_list_head;
+    mm->sm_priv = &pra_list_head;
     return 0;
 }
 /*
@@ -54,6 +56,10 @@ static int _clock_map_swappable(struct mm_struct* mm, uintptr_t addr, struct Pag
     // link the most recent arrival page at the back of the pra_list_head qeueue.
     // 将页面page插入到页面链表pra_list_head的末尾
     // 将页面的visited标志置为1，表示该页面已被访问
+    //list_add_before(&pra_list_head,entry);
+    list_add_before((list_entry_t*) mm->sm_priv,entry);
+    page->visited = 1;
+    return 0;
     return 0;
 }
 /*
@@ -76,6 +82,25 @@ static int _clock_swap_out_victim(struct mm_struct* mm, struct Page** ptr_page, 
         // 获取当前页面对应的Page结构指针
         // 如果当前页面未被访问，则将该页面从页面链表中删除，并将该页面指针赋值给ptr_page作为换出页面
         // 如果当前页面已被访问，则将visited标志置为0，表示该页面已被重新访问
+         curr_ptr = list_next(curr_ptr);
+        if(curr_ptr == head) {
+         curr_ptr = list_next(curr_ptr);
+            if(curr_ptr == head) {
+                *ptr_page = NULL;
+                break;
+            }
+        }
+
+        struct Page* page = le2page(curr_ptr, pra_page_link);
+        if(!page->visited) {
+            *ptr_page = page;
+            list_del(curr_ptr);
+            cprintf("curr_ptr %p\n",curr_ptr);
+            //curr_ptr = head;
+            break;
+        } else {
+            page->visited = 0;
+        }
     }
     return 0;
 }
